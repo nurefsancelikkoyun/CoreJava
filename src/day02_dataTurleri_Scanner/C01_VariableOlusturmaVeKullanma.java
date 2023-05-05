ef060071fc5e8a31a7f633df7b753d4ad8648b62
Nurefşan <nurrcelikk50@gmail.com>
2023-05-06 00:29:56 +0300

    2.gun

---
+++ src/day02_dataTurleri_Scanner/C01_VariableOlusturmaVeKullanma.java
@@ -0,0 +1,32 @@
+package day02_dataTurleri_Scanner;
+
+public class C01_VariableOlusturmaVeKullanma {
+    public static void main(String[] args) {
+
+        /*
+            Java RunTime programdir
+            Yani biz yesil OK'a basip calisytirdigimizda
+            hersey sifirdan baslar
+
+            class'in sonuna kadar tum kodlar
+            yukaridan asagi calistirilir
+
+            class bittiginde hersey SIFIRLANIR
+         */
+
+        int bakiye=0;
+
+
+
+        bakiye=200;
+
+        System.out.println( bakiye ); // 200
+
+        bakiye=500;
+
+
+        bakiye=300;
+
+        System.out.println( bakiye ); // 300
+    }
+}
