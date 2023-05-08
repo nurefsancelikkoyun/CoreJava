f5314624be7b255c7e8684a64acb877c76b7e23b
Nurefşan <nurrcelikk50@gmail.com>
2023-05-08 23:49:11 +0300

    4.gun

--- src/day02_dataTurleri_Scanner/C02_DataTurleri.java
+++ src/day02_dataTurleri_Scanner/C02_DataTurleri.java
@@ -3,28 +3,30 @@ package day02_dataTurleri_Scanner;
 public class C02_DataTurleri {
     public static void main(String[] args) {
 
-        char ilkHarf= '8'; // Tek tirnak icinde tek karakter(harf,sayi,ozel karakter...)
+        /*
+            Java RunTime programdir
+            Yani biz yesil OK'a basip calisytirdigimizda
+            hersey sifirdan baslar
+
+            class'in sonuna kadar tum kodlar
+            yukaridan asagi calistirilir
+
+            class bittiginde hersey SIFIRLANIR
+         */
+
+        int bakiye=0;
 
 
-        byte ogrYasi1=15; // en dogru kullanim bu olur, hafiza kullanimini optimize etmek icin bu yeterlidir
-        short ogryasi2=16;
-        int ogrYasi3=14;
-        long ogrYasi4=15;
 
-        double sayi1=3.1234567890123453789;
-        System.out.println(sayi1); // 3.1234567890123457
+        bakiye=200;
 
-        // Double virgulden sonra 16 hane yazdirir, ama cok kucuk sayilara indigi icin
-        // ondalikli kismin sonunda hatalar olabilir
-        // 15 / 3 = 4.999999999999999  5.0  5.00000000000001
+        System.out.println( bakiye ); // 200
 
-        float sayi2 = 45.543334578465354648f;
-        // float sayi tanimladigimizda double'dan farkli oldugunu belirtmek icin
-        // sonuna f veya F yazmak zorundayiz
-        System.out.println(sayi2); // 45.543335
+        bakiye=500;
 
 
-        // Biz dersler boyunca hafiza onemli olmadigi icin
-        // tamsayilar icin int, ondalikli sayilar icin de double kullaniriz
+        bakiye=300;
+
+        System.out.println( bakiye ); // 300
     }
 }
